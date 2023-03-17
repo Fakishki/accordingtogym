e42d562fb21f273c0b29e6e6bf697428c1de4b86
Wally Pfingsten <wallypfingsten@gmail.com>
2023-03-17 03:48:18 -0700

    site running added db.json but not updated App.js yet

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExerciseTracker from './ExerciseTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <ExerciseTracker />
+    </div>
+  );
+}
+
+export default App;
